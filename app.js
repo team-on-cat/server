4668014d64796d04bcef20ae4fcca3ad182bf548
Arief Rachman <arievrchman@gmail.com>
2019-03-15 09:28:59 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+let db_connect = 'mongodb://localhost/teamoncat';
+mongoose.connect(db_connect, { useNewUrlParser: true });
+mongoose.connection.once('open', () => console.log('Database connected')).on('error', (error) => console.log('connection error:', error));
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;

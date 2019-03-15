a22dff50af28c0c2cd6977751488b6f4741e8fd2
isrotrip <isrotrip93@gmail.com>
2019-03-15 09:48:07 +0700

    add middleware to multer and upload file into GCS

---
+++ middlewares/images.js
@@ -0,0 +1,64 @@
+require('dotenv').config()
+
+const { Storage } = require('@google-cloud/storage')
+
+const CLOUD_BUCKET = process.env.CLOUD_BUCKET
+
+const storage = new Storage({
+  projectId: process.env.GCLOUD_PROJECT,
+  keyFilename: process.env.KEYFILE_PATH
+})
+
+const bucket = storage.bucket(CLOUD_BUCKET)
+
+const getPublicUrl = (filename) => {
+  return `https://storage.googleapis.com/${CLOUD_BUCKET}/${filename}`
+}
+
+const sendUploadToGCS = (req, res, next) => {
+  if (!req.file) {
+    return next()
+  }
+
+  const gcsname = Date.now() + req.file.originalname
+  const file = bucket.file(gcsname)
+
+  const stream = file.createWriteStream({
+    metadata: {
+      contentType: req.file.mimetype
+    }
+  })
+
+  stream.on('error', (err) => {
+    req.file.cloudStorageError = err
+    next(err)
+  })
+
+  stream.on('finish', () => {
+    req.file.cloudStorageObject = gcsname
+    file.makePublic().then(() => {
+      if(req.file){
+        req.file.cloudStoragePublicUrl = getPublicUrl(gcsname)
+      } else {
+        req.file = ''
+      }
+      next()
+    })
+  })
+
+  stream.end(req.file.buffer)
+}
+
+const Multer = require('multer'),
+      multer = Multer({
+        storage: Multer.MemoryStorage,
+        limits: {
+          fileSize: 5 * 1024 * 1024
+        }
+      })
+
+module.exports = {
+  getPublicUrl,
+  sendUploadToGCS,
+  multer
+}
